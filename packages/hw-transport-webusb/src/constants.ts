084969bd4a62fb2ee0cb385294112641e343dd47
aaronchen <aaron@keyst.one>
2024-09-11 10:47:27 +0800

    Update package versions and scripts for development and production environments in hw-transport-webusb.

--- packages/hw-transport-webusb/src/constants.ts
+++ packages/hw-transport-webusb/src/constants.ts
@@ -1,5 +1,7 @@
 export const USBConfigurationValue = 1;
-export const USBInterfaceNumber = process.env.NODE_ENV === 'production' ? 0 : 1;
+// use 0 for production firmware
+// use 1 for development firmware since the 0 interface number will be used by MSC for development
+export const USBInterfaceNumber = process.env.KEYSTONE_USB_ENV === 'development' ? 1 : 0;
 export const USBPackageSize = 64;
 export const MAXUSBPackets = 200;
 // 15s to set the usb timeout since it may need users' action on the device.
