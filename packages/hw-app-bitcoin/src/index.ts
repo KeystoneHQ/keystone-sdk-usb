c18b181f137289d0b278feed6fe5b2937446ca38
rea <hi@rea.ink>
2024-10-31 17:34:18 +0800

    feat: add p2tr version, support sign by path

--- packages/hw-app-bitcoin/src/index.ts
+++ packages/hw-app-bitcoin/src/index.ts
@@ -1,21 +1,22 @@
 import * as uuid from 'uuid';
-import Base, { parseResponoseUR } from "@keystonehq/hw-app-base";
+import Base, { parseResponoseUR } from '@keystonehq/hw-app-base';
 import { Actions, TransportWebUSB, type TransportConfig } from '@keystonehq/hw-transport-webusb';
 import { Curve, CryptoPSBT, DerivationAlgorithm, CryptoMultiAccounts } from '@keystonehq/bc-ur-registry';
 import { BtcSignature, BtcSignRequest, DataType } from '@keystonehq/bc-ur-registry-btc';
-import { HDKey } from "@scure/bip32";
+import { HDKey } from '@scure/bip32';
 import * as bitcoin from 'bitcoinjs-lib';
 import { PartialSignature } from './psbt';
-import { UREncoder, UR } from '@ngraveio/bc-ur';
+import { UREncoder, UR, URDecoder } from '@ngraveio/bc-ur';
+import { Status, throwTransportError } from '@keystonehq/hw-transport-error';
 
 
 const decodePath = (path: string) => {
     const paths = path.replace(/[m|M]\//, '').split('/');
     return paths.map(path => {
-        let decodedPath = path.split('\'')
-        return { pathIndex: parseInt(decodedPath[0]), hardened: decodedPath[1] ? true : false }
-    })
-}
+        const decodedPath = path.split('\'');
+        return { pathIndex: parseInt(decodedPath[0]), hardened: decodedPath[1] ? true : false };
+    });
+};
 
 
 enum NetworkType {
@@ -44,7 +45,10 @@ const VersionMap = {
             private: 0x04b2430c,
             public: 0x04b24746,
         },
-
+        [ScriptType.p2tr]: {
+            private: 0x0488ADE4,
+            public: 0x0488b21e,
+        },
     },
     [NetworkType.testnet]: {
         [ScriptType.p2pkh]: {
@@ -59,27 +63,29 @@ const VersionMap = {
             private: 0x045f18bc,
             public: 0x045f1cf6,
         },
-        // TODO: add p2tr
+        [ScriptType.p2tr]: {
+            private: 0x04358394,
+            public: 0x043587cf,
+        },
     },
-}
+};
 
 
 const getVersion = (purpose: number, coinType: number) => {
-    if (purpose === ScriptType.p2tr) {
-        throw new Error("p2tr is not supported");
-    }
-
     const networkType = coinType === 0 ? NetworkType.mainnet : NetworkType.testnet;
-    const scriptType = purpose === ScriptType.p2pkh ? ScriptType.p2pkh :
-        purpose === ScriptType.p2sh ? ScriptType.p2sh :
-            purpose === ScriptType.p2wpkh ? ScriptType.p2wpkh : null;
+    const scriptType = {
+        [ScriptType.p2pkh]: ScriptType.p2pkh,
+        [ScriptType.p2sh]: ScriptType.p2sh,
+        [ScriptType.p2wpkh]: ScriptType.p2wpkh,
+        [ScriptType.p2tr]: ScriptType.p2tr,
+    }[purpose] || null;
 
     if (scriptType === null) {
-        throw new Error("Invalid purpose");
+        throw new Error('Invalid purpose');
     }
 
     return VersionMap[networkType][scriptType];
-}
+};
 
 
 
@@ -95,24 +101,32 @@ class Bitcoin extends Base {
         super(transport, mfp);
     }
 
+    static async createWithUSBTransport(
+        config?: TransportConfig,
+    ): Promise<Bitcoin> {
+        await TransportWebUSB.requestPermission();
+        const transport = await TransportWebUSB.connect(config);
+        await transport.close();
+        return new Bitcoin(transport);
+    }
 
-    async getExtendedPublicKey(path: string, display: boolean = true): Promise<string> {
+    async getExtendedPublicKey(path: string, display = true): Promise<string> {
         // decode path
         const pathArray = decodePath(path);
         // check if path is valid
         if (pathArray.length <= 0) {
-            throw new Error("Invalid path")
+            throw new Error('Invalid path');
         }
 
         // get coin index
         const purpopse = pathArray[0].pathIndex;
         const coinType = pathArray[1].pathIndex;
         if (purpopse != 44 && purpopse != 49 && purpopse !== 84 && purpopse !== 86 && purpopse !== 1) {
-            throw new Error("Invalid coin index")
+            throw new Error('Invalid coin index');
         }
 
         if (coinType != 0 && coinType != 1) {
-            throw new Error("Invalid coin type")
+            throw new Error('Invalid coin type');
         }
 
         const version = getVersion(purpopse, coinType);
@@ -125,8 +139,8 @@ class Bitcoin extends Base {
         const hdkey = new HDKey({
             publicKey: pubkey,
             chainCode: chainCode,
-            versions: version
-        })
+            versions: version,
+        });
         this.mfp = account.getMasterFingerprint().toString('hex');
         return hdkey.publicExtendedKey;
     }
@@ -141,11 +155,19 @@ class Bitcoin extends Base {
      * it should not be the rotation psbt which will have mutiple signature for one input
      * the cordorator should send the same psbt to each signer and collect the result
      */
-    async signPsbt(psbt: string | Buffer): Promise<[number, PartialSignature][]> {
-        const signedPsbtB64 = await this.signPsbtRaw(psbt);
+    async signPsbt(psbt: string | Buffer, path?: string): Promise<[number, PartialSignature][]> {
+        const signedPsbtB64 = await this.signPsbtRaw(psbt, path);
         const psbtObj = bitcoin.Psbt.fromBase64(signedPsbtB64);
+
         return psbtObj.data.inputs.map((input, index) => {
             const inputIndex = index;
+
+            const hasPath = [input.tapBip32Derivation, input.bip32Derivation]
+                .filter(it => it)
+                .some(d => d?.[0]?.path.startsWith(`m/${path}`));
+
+            if (!hasPath) return null;
+
             // caution: the partialSig will just using the first one for each input
             // this is for single signing case, for multisig,
             // the cordorator should send the same psbt to each signer 
@@ -153,18 +175,36 @@ class Bitcoin extends Base {
                 const partialSignature = input.partialSig[0];
                 const pubkey = partialSignature.pubkey;
                 const signature = partialSignature.signature;
+
                 if (input.tapBip32Derivation && input.tapBip32Derivation.length > 0) {
                     const tapLeafHash = input.tapBip32Derivation[0].leafHashes;
                     if (tapLeafHash.length > 0) {
-                        return [Number(inputIndex), new PartialSignature(Buffer.from(pubkey), Buffer.from(signature), Buffer.from(tapLeafHash[0]))];
+                        return [
+                            Number(inputIndex),
+                            new PartialSignature(
+                                Buffer.from(pubkey),
+                                Buffer.from(signature),
+                                Buffer.from(tapLeafHash[0])),
+                        ];
                     }
                 }
 
                 return [Number(inputIndex), new PartialSignature(Buffer.from(pubkey), Buffer.from(signature))];
             }
 
-            throw new Error("No signature found for input " + inputIndex);
-        })
+            if (input.tapKeySig) {
+                const pubkey = input.tapInternalKey;
+                const signature = input.tapKeySig;
+
+                return [inputIndex, new PartialSignature(
+                    Buffer.from(pubkey!),
+                    Buffer.from(signature)
+                )];
+            }
+
+            throw new Error('No signature found for input ' + inputIndex);
+        }).filter(it => it) as [number, PartialSignature][];
+
     }
 
     /**
@@ -174,16 +214,29 @@ class Bitcoin extends Base {
      * @throws Will throw an error if the PSBT is invalid or if the signing process fails.
      * @caution This function provide much more flexiblity for caller to handle the signed psbt
      */
-    async signPsbtRaw(psbt: string | Buffer): Promise<string> {
+    async signPsbtRaw(psbt: string | Buffer, path?: string): Promise<string> {
         if (typeof psbt === 'string') {
-            psbt = Buffer.from(psbt, "base64");
+            psbt = Buffer.from(psbt, 'base64');
         }
-        const psbtUR = new CryptoPSBT(psbt).toUR();
+        const psbtObj = bitcoin.Psbt.fromBuffer(psbt);
+        const clearInputs = psbtObj.data.inputs.map((it, index) => {
+            const hasPath = [it.tapBip32Derivation, it.bip32Derivation]
+                .filter(it => it)
+                .some(d => d?.[0]?.path.startsWith(`m/${path}`));
+            if (hasPath) {
+                return null;
+            }
+            return index;
+        }).filter(it => it);
+        for (const index of Object.values(clearInputs)) {
+            psbtObj.data.inputs[index!].partialSig = [];
+        }
+        const psbtUR = new CryptoPSBT(Buffer.from(psbtObj.toBuffer())).toUR();
         const encodedUR = new UREncoder(psbtUR, Infinity).nextPart().toUpperCase();
         const response = await this.sendToDevice(Actions.CMD_RESOLVE_UR, encodedUR);
-        let resultUR = parseResponoseUR(response.payload) as unknown as CryptoPSBT;
-        let signedPsbt = resultUR.getPSBT();
-        return signedPsbt.toString('base64')
+        const resultUR = parseResponoseUR(response.payload);
+        const signedPsbt = CryptoPSBT.fromCBOR(resultUR.cbor).getPSBT();
+        return signedPsbt.toString('base64');
     }
 
     /**
@@ -192,8 +245,12 @@ class Bitcoin extends Base {
      * @throws Will throw an error if the device communication fails or if the response cannot be parsed
      */
     async getMasterFingerprint(): Promise<string> {
-        const result = await this.getAppConfig()
+        const result = await this.getAppConfig();
         return result.mfp;
+    }
+
+    setMasterFingerprint(mfp: string): void {
+        this.mfp = mfp;
     }
 
     /**
@@ -209,25 +266,25 @@ class Bitcoin extends Base {
        */
     async signMessage(message: string, path: string): Promise<string> {
         this.precheck();
-        const messageBuffer = Buffer.from(message, "utf8");
+        const messageBuffer = Buffer.from(message, 'utf8');
         const encodedUR = constructURRequest(messageBuffer, path, this.mfp as string, DataType.message);
         const response = await this.sendToDevice(Actions.CMD_RESOLVE_UR, encodedUR);
-        return parseSignatureUR(response.payload)
+        return parseSignatureFromURPayload(response.payload);
     }
 
 }
 
 function constructURRequest(messageBuffer: Buffer, path: string, mfp: string, type: DataType): string {
     const requestId = uuid.v4();
-    let xfps = [mfp]
-    let paths = [path]
-    let cosmosUR = BtcSignRequest.constructBtcRequest(
+    const xfps = [mfp];
+    const paths = [path];
+    const cosmosUR = BtcSignRequest.constructBtcRequest(
         requestId,
         xfps,
         messageBuffer,
         type,
         paths
-    )
+    );
 
     const ur = cosmosUR.toUR();
     const encodedUR = new UREncoder(ur, Infinity).nextPart().toUpperCase();
@@ -235,11 +292,18 @@ function constructURRequest(messageBuffer: Buffer, path: string, mfp: string, ty
 }
 
 
-const parseSignatureUR = (ur: UR) => {
-    let signature = BtcSignature.fromCBOR(ur.cbor)
-    let signatureBuffer = signature.getSignature();
+const parseSignatureFromURPayload = (urPlayload: string) => {
+    const decoder = new URDecoder();
+    decoder.receivePart(urPlayload);
+    if (!decoder.isComplete()) {
+        throwTransportError(Status.ERR_UR_INCOMPLETE);
+    }
+    const resultUR = decoder.resultUR();
+
+    const signature = BtcSignature.fromCBOR(resultUR.cbor);
+    const signatureBuffer = signature.getSignature();
     return signatureBuffer.toString('base64');
-}
+};
 
 
 export default Bitcoin;
