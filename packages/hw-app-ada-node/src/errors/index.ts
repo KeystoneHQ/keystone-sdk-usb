330b6d09b0590b3550d7c945ba0af83f1d0b0f5b
ZhenQian <zhenqian@keyst.one>
2024-12-19 11:36:37 +0800

    refactor: remove hw-ada-node

--- packages/hw-app-ada-node/src/errors/index.ts
+++
@@ -1,9 +0,0 @@
-export { ErrorBase } from './errorBase';
-export { InvalidData } from './invalidData';
-export { DeviceVersionUnsupported } from './deviceUnsupported';
-export {
-  DeviceStatusCodes,
-  DeviceStatusError,
-  DeviceStatusMessages,
-} from './deviceStatusError';
-export { InvalidDataReason } from './invalidDataReason';
