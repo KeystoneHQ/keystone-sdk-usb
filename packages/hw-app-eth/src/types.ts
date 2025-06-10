009c36c4fb0a56caa596008583aae3f629b690c6
soralit <soralitria@gmail.com>
2025-06-10 17:01:17 +0800

    feat: support get multiple keys with usb

--- packages/hw-app-eth/src/types.ts
+++ packages/hw-app-eth/src/types.ts
@@ -1,8 +1,18 @@
-import { Chain } from '@keystonehq/hw-transport-usb';
-import { Wallet } from './request';
+import { Chain } from "@keystonehq/hw-transport-usb";
+import { Wallet } from "./request";
 
 export type ExportPubKeyParams = {
   chain?: Chain;
   Wallet?: Wallet;
   [key: string]: any;
-}
+};
+
+export type Address = {
+  address: string;
+  publicKey: string;
+  chainCode?: string;
+};
+
+export type ExtendedPubkey = Address & {
+  xpub: string;
+};
