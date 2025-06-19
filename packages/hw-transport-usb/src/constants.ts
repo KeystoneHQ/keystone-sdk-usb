9b55d348f8ca0e5d258c2cc50bea7a9e641b9591
soralit <soralitria@gmail.com>
2025-06-19 17:36:13 +0800

    fix: error issue in web env

--- packages/hw-transport-usb/src/constants.ts
+++ packages/hw-transport-usb/src/constants.ts
@@ -1,7 +1,12 @@
 export const USBConfigurationValue = 1;
 // use 0 for production firmware
 // use 1 for development firmware since the 0 interface number will be used by MSC for development
-export const USBInterfaceNumber = process.env.KEYSTONE_USB_ENV === 'development' ? 1 : 0;
+export const USBInterfaceNumber =
+  typeof window !== "undefined"
+    ? 0
+    : process.env.KEYSTONE_USB_ENV === "development"
+    ? 1
+    : 0;
 export const USBPackageSize = 64;
 export const MAXUSBPackets = 200;
 // 15s to set the usb timeout since it may need users' action on the device.
